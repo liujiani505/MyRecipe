8b8f0acc93e5a432701cc2acf7728a443feddf8f
jianiliu <liujiani505@gmail.com>
2022-11-02 15:51:59 -0400

    adding a loading spinner

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -15,6 +15,8 @@ export class AuthComponent {
     }
 
     isLoginMode = true;
+    isLoading = false;
+    error:string = null;
 
     onSwitchMode(){
         this.isLoginMode = !this.isLoginMode;
@@ -28,19 +30,24 @@ export class AuthComponent {
         const email= form.value.email;
         const password = form.value.password;
 
+        this.isLoading = true;
+
         if(this.isLoginMode){
             // ....
         } else {
 
             this.authService.signup(email, password)
             .subscribe(resData => {
-                console.log(resData)
+                console.log(resData);
+                this.isLoading = false;
             },
                 error => {
-                    console.log(error)
+                    console.log(error);
+                    this.error = 'An error occurred!';
+                    this.isLoading = false;
                 }
             );
-            
+
         }
 
         form.reset();
