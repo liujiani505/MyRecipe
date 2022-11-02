ab536496612f83b6966407541a5d943f6d7ec2b0
jianiliu <liujiani505@gmail.com>
2022-11-02 15:10:50 -0400

    send signup request

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
     selector: 'app-auth',
     templateUrl: './auth.component.html'
 })
 
+
 export class AuthComponent {
+
+    constructor(private authService: AuthService){
+    
+    }
+
     isLoginMode = true;
 
     onSwitchMode(){
@@ -14,11 +21,28 @@ export class AuthComponent {
     }
 
     onSubmit(form: NgForm){
+
         if(!form.valid){
             return;
         }
         const email= form.value.email;
         const password = form.value.password;
+
+        if(this.isLoginMode){
+            // ....
+        } else {
+
+            this.authService.signup(email, password)
+            .subscribe(resData => {
+                console.log(resData)
+            },
+                error => {
+                    console.log(error)
+                }
+            );
+            
+        }
+
         form.reset();
     }
 }
