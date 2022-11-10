e2cd994cbbaefd1c5c1aae6fcecc4b6fa65b8660
jianiliu <liujiani505@gmail.com>
2022-11-10 15:07:10 -0500

    preparing programmatic creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.componenet';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { AlertComponent } from './shared/alert/alert.component';
+import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 
 @NgModule({
   declarations: [
@@ -36,7 +37,9 @@ import { AlertComponent } from './shared/alert/alert.component';
     RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
-    AlertComponent
+    AlertComponent,
+    AlertComponent,
+    PlaceholderDirective
   ],
   imports: [
     BrowserModule,
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 import { AuthResponseData } from './auth.service';
+import { AlertComponent } from '../shared/alert/alert.component';
+
 
 @Component({
     selector: 'app-auth',
@@ -13,7 +15,7 @@ import { AuthResponseData } from './auth.service';
 
 export class AuthComponent {
 
-    constructor(private authService: AuthService, private router: Router){
+    constructor(private authService: AuthService, private router: Router, private componentFactoryResolver: ComponentFactoryResolver){
     
     }
 
@@ -52,6 +54,7 @@ export class AuthComponent {
             errorMessage => {
                 console.log(errorMessage);
                 this.error = errorMessage;
+                this.showErrorAlert(errorMessage);
                 this.isLoading = false;
             }
         );
@@ -61,5 +64,10 @@ export class AuthComponent {
 
     onHandleError(){
         this.error = null;
+    }
+
+    showErrorAlert(message: string){
+        const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+        
     }
 }
