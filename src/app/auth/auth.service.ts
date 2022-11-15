5d14f6e3e650b04b55395bda1fdc0c7f13605058
jianiliu <liujiani505@gmail.com>
2022-11-15 12:15:19 -0500

    import environment variable into auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,7 +4,7 @@ import { catchError, tap } from "rxjs/operators";
 import { throwError, Subject, BehaviorSubject } from "rxjs" ;
 import { User } from "./user.model";
 import { Router } from '@angular/router';
-import { ignoreElements } from "rxjs-compat/operator/ignoreElements";
+import { environment } from "src/environments/environment";
 
 
 export interface AuthResponseData{
@@ -34,7 +34,7 @@ export class AuthService {
     constructor(private http: HttpClient, private router: Router){}
 
     signup(email:string, password: string){
-        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBz91hzQftaBmDTj-6Smvk51_-U9BZ8hdY', 
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAIPKey, 
         { 
             email: email, 
             password: password, 
@@ -48,7 +48,7 @@ export class AuthService {
     }
 
     login(email: string, password: string){
-        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBz91hzQftaBmDTj-6Smvk51_-U9BZ8hdY',
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAIPKey,
         { 
             email: email, 
             password: password, 
