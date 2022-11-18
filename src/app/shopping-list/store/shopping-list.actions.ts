f32689f558615c41eba60ae826524bccd7a4ae7d
jianiliu <liujiani505@gmail.com>
2022-11-18 16:16:51 -0500

    dispatching actions

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -8,5 +8,7 @@ export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 // Action is an interface
 export class AddIngredient implements Action {
     readonly type = ADD_INGREDIENT;  // adding typescript annotation readonly to indicate that this should never be changed from outside.
-    payload: Ingredient  //payload is not a name you have to use, the Action interface only forces you to add a "type" property
+    // payload: Ingredient  //payload is not a name you have to use, the Action interface only forces you to add a "type" property
+    // instead of adding payload as a property, we can add a constructor function where we accept a payload argument so in shopping list edit component, we can pass newingredient as an argument to dispatch an action.
+    constructor(public payload: Ingredient) {}
 }
