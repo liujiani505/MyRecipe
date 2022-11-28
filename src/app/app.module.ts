d2acde1f42873cd420dbe8651ba69cde69460b4b
jianiliu <liujiani505@gmail.com>
2022-11-28 14:13:43 -0500

    one root state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 import { StoreModule } from '@ngrx/store';
 import { LoggingService } from './logging.service';
-import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
+import * as fromApp from '../app/store/app.reducer';
 
 
 @NgModule({
@@ -23,7 +23,7 @@ import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer
     HttpClientModule,
     SharedModule,
     CoreModule,
-    StoreModule.forRoot({shoppingList: shoppingListReducer})
+    StoreModule.forRoot(fromApp.appReducer)
   ],
   providers: [LoggingService],
   bootstrap: [AppComponent]
