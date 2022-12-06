e8e0f7692138a97c1c2c32d14d0d3d0c50f5cc26
jianiliu <liujiani505@gmail.com>
2022-12-06 16:48:34 -0500

    adding recipes actions

--- src/app/recipes/store/recipe.reducer.ts
+++ src/app/recipes/store/recipe.reducer.ts
@@ -1,5 +1,5 @@
-import { Recipe } from '../recipe.model'
-
+import { Recipe } from '../recipe.model';
+import * as RecipesActions from './recipe.actions'
 
 export interface State {
     recipes: Recipe[],
@@ -9,6 +9,14 @@ const initialState: State = {
     recipes: []
 }
 
-export function recipeReducer(state: State, action){
-     return state;
+export function recipeReducer(state = initialState, action: RecipesActions.RecipesActions){
+     switch(action.type){
+        case RecipesActions.SET_RECIPES:
+            return {
+                ...state,
+                recipes: [...action.payload]
+            }
+        default:
+            return state;
+     }
 }
