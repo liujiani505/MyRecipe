df18e540b41b8a7799ce2247b014b8878cb0b941
jianiliu <liujiani505@gmail.com>
2022-12-06 16:57:12 -0500

    getting started with ngrx for recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit, OnDestroy} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Recipe } from '../recipe.model'
-import { RecipeService } from '../recipe.service';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../../store/app.reducer'
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-recipe-list',
@@ -15,16 +17,15 @@ export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
   subscription: Subscription;
 
-  constructor(private recipeService: RecipeService, private router: Router, private route: ActivatedRoute) { }
+  constructor(private router: Router, private route: ActivatedRoute, private store: Store<fromApp.AppState>) { }
 
   ngOnInit(){
-   this.subscription = this.recipeService.recipesChanged
+   this.subscription = this.store.select('recipes')
+   .pipe(map(recipesState => recipesState.recipes)) 
     .subscribe(
       (recipes: Recipe[])=>{
       this.recipes = recipes;
-    })
-    this.recipes = this.recipeService.getRecipes()
-    ;
+    });
   }
 
   onNewRecipe(){
